d49af1c4780c69bb9425c7b6d2f64fc6dcaac85a
Matheus A. Araujo <matheusaraujotradutor@gmail.com>
2026-01-19 01:32:16 -0300

    created first test

---
+++ test_simple.py
@@ -0,0 +1,11 @@
+user = password = None
+
+with open("credential.txt", encoding="utf-8") as f:
+    for line in f:
+        if line.startswith("user:"):
+            user = line[5:].strip()
+        elif line.startswith("pass:"):
+            password = line[5:].strip()
+
+if user is None or password is None:
+    raise ValueError("Missing credentials")
